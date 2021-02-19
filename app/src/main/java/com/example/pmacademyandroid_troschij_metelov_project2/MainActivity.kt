6da0f4c187984a169af668cf2cc5efb599cf55e9
Roman Metelov <rm0734861369@gmail.com>
2021-02-19 17:04:42 +0200

    dgjdfgjdkjfngdjk

--- app/src/main/java/com/example/pmacademyandroid_troschij_metelov_project2/MainActivity.kt
+++ app/src/main/java/com/example/pmacademyandroid_troschij_metelov_project2/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     //Commentary
+    //ищщо commentary
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
